9e56884090fb415c027ffe2438c57a2935b8c976
Fransi <flsotz@gmail.com>
2023-10-25 21:00:17 -0600

    Implmentando Crud Fabricnte

--- src/main/java/com/sourcecode/apparticulos/services/FabricanteServiceImpl.java
+++ src/main/java/com/sourcecode/apparticulos/services/FabricanteServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 @Service
@@ -44,6 +45,16 @@ public class FabricanteServiceImpl implements IFabricanteService {
         FabricanteResponseRest fabricanteResponseRest = new FabricanteResponseRest();
         List<Fabricante> fabricanteList = new ArrayList<>();
         try {
+            Optional<Fabricante> fabricanteOptional = iFabricanteDao.findById(id);
+            if (fabricanteOptional.isPresent()) {
+                fabricanteList.add(fabricanteOptional.get());
+                fabricanteResponseRest.getFabricanteResponse().setFabricanteList(fabricanteList);
+                fabricanteResponseRest.setMetadata("Respuesta Exitosa", "200", "Fabriante Encontrado");
+            } else {
+                log.severe("no se encontro el Fabricante con id " + id);
+                fabricanteResponseRest.setMetadata("Respuesta no Exitosa", "404", "Fabricante con id " + id + "No encontrado");
+                new ResponseEntity<FabricanteResponseRest>(fabricanteResponseRest, HttpStatus.NOT_FOUND);
+            }
         } catch (Exception exception) {
             log.severe("Error al buscar Fabricante" + exception.getMessage());
             exception.getStackTrace();
@@ -54,17 +65,86 @@ public class FabricanteServiceImpl implements IFabricanteService {
     }
 
     @Override
+    @Transactional
     public ResponseEntity<FabricanteResponseRest> crearFabricante(Fabricante fabricante) {
-        return null;
+        log.info("Ejecutando crearFabricante(Fabricante fabricante)");
+        FabricanteResponseRest fabricanteResponseRest = new FabricanteResponseRest();
+        List<Fabricante> fabricanteList = new ArrayList<>();
+        try {
+            Fabricante fabricanteGuardado = iFabricanteDao.save(fabricante);
+            if (fabricanteGuardado != null) {
+                fabricanteList.add(fabricanteGuardado);
+                fabricanteResponseRest.getFabricanteResponse().setFabricanteList(fabricanteList);
+                fabricanteResponseRest.setMetadata("Respuesta Exitosa", "200", "Fabricante Guardado");
+            } else {
+                log.severe("No se creo el Fabricante");
+                fabricanteResponseRest.setMetadata("Error al guardar al Fabricante", "500", "Fabricante no Guardado");
+                return new ResponseEntity<FabricanteResponseRest>(fabricanteResponseRest, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } catch (Exception exception) {
+            log.severe("Error al guardar Fabricante" + exception.getMessage());
+            exception.getStackTrace();
+            fabricanteResponseRest.setMetadata("Errog al Guardar Articulo", "500", "Articulo no guardado");
+            return new ResponseEntity<FabricanteResponseRest>(fabricanteResponseRest, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<FabricanteResponseRest>(fabricanteResponseRest, HttpStatus.OK);
     }
 
     @Override
+    @Transactional
     public ResponseEntity<FabricanteResponseRest> actualizarFabricante(Fabricante fabricante, Long id) {
-        return null;
+        log.info("Ejecutando actualizarFabricante(Fabricante fabricante, Long id)");
+        FabricanteResponseRest fabricanteResponseRest = new FabricanteResponseRest();
+        List<Fabricante> fabricanteList = new ArrayList<>();
+        try {
+            Optional<Fabricante> fabricanteOptionalBuscado = iFabricanteDao.findById(id);
+            if (fabricanteOptionalBuscado.isPresent()) {
+                fabricanteOptionalBuscado.get().setNombre(fabricante.getNombre());
+                Fabricante fabricanteActualizado = iFabricanteDao.save(fabricanteOptionalBuscado.get());
+                if (fabricanteActualizado != null) {
+                    fabricanteList.add(fabricanteActualizado);
+                    fabricanteResponseRest.getFabricanteResponse().setFabricanteList(fabricanteList);
+                    fabricanteResponseRest.setMetadata("Respuesta Exitosa", "200", "Fabricante Actualizado");
+                } else {
+                    log.severe("No se actualizo Fabricante ");
+                    fabricanteResponseRest.setMetadata("Error al actualizar Fabricante", "500", "Fabricante no Actualizado");
+                    return new ResponseEntity<FabricanteResponseRest>(fabricanteResponseRest, HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+            } else {
+                log.severe("No se actualizo Fabricante con id " + id);
+                fabricanteResponseRest.setMetadata("Respuesta no Exitosa", "404", "Fabricante " + id + "No actualizado");
+                return new ResponseEntity<FabricanteResponseRest>(fabricanteResponseRest, HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception exception) {
+            log.severe("Error al actualizar Fabricante" + exception.getMessage());
+            exception.getStackTrace();
+            fabricanteResponseRest.setMetadata("Error al actualizar Fabricante", "500", "Fabricante no Actuzlizado");
+            return new ResponseEntity<FabricanteResponseRest>(fabricanteResponseRest, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<FabricanteResponseRest>(fabricanteResponseRest, HttpStatus.OK);
     }
 
     @Override
+    @Transactional
     public ResponseEntity<FabricanteResponseRest> eliminarFabricante(Long id) {
-        return null;
+        log.info("Ejecutando eliminarFabricante(Long id)");
+        FabricanteResponseRest fabricanteResponseRest = new FabricanteResponseRest();
+        try {
+            Optional<Fabricante> fabricanteOptionalBuscado = iFabricanteDao.findById(id);
+            if (fabricanteOptionalBuscado.isPresent()) {
+                iFabricanteDao.delete(fabricanteOptionalBuscado.get());
+                fabricanteResponseRest.setMetadata("Respuesta Exitosa", "200", "Fabricante Eliminado");
+            } else {
+                log.severe("No se encontro Fabricante con id" + id);
+                fabricanteResponseRest.setMetadata("Respuesta No Exitosa", "404", "Fabricante " + id + "No encontrado");
+                return new ResponseEntity<FabricanteResponseRest>(fabricanteResponseRest, HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception exception) {
+            log.severe("Error al eliminar Fabricante" + exception.getMessage());
+            exception.getStackTrace();
+            fabricanteResponseRest.setMetadata("Error al eliminar Fabricante", "500", "Fabricante no Eliminaro");
+            return new ResponseEntity<FabricanteResponseRest>(fabricanteResponseRest, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<FabricanteResponseRest>(fabricanteResponseRest, HttpStatus.OK);
     }
 }
